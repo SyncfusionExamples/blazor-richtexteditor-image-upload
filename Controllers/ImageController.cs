e62242c0e0f6e70ef8d56b27febe3186f9164b02
Rajendran R <rajendranr-5483@users.noreply.github.com>
2020-07-27 19:29:07 +0530

    Committed files to run the application.

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace ImageUpload.Controllers
+{
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        private readonly IWebHostEnvironment hostingEnv;
+
+        public ImageController(IWebHostEnvironment env)
+        {
+            this.hostingEnv = env;
+        }
+
+        [HttpPost("[action]")]
+        [Route("api/Image/Save")]
+        public void Save(IList<IFormFile> UploadFiles)
+        {
+            try
+            {
+                foreach (var file in UploadFiles)
+                {
+                    if (UploadFiles != null)
+                    {
+                        string targetPath = hostingEnv.ContentRootPath + "\\wwwroot\\Images";
+                        string filename = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+
+                        // Create a new directory, if it does not exists
+                        if (!Directory.Exists(targetPath))
+                        {
+                            Directory.CreateDirectory(targetPath);
+                        }
+
+                        // Name which is used to save the image
+                        filename = targetPath + $@"\{filename}";
+
+                        if (!System.IO.File.Exists(filename))
+                        {
+                            // Upload a image, if the same file name does not exist in the directory
+                            using (FileStream fs = System.IO.File.Create(filename))
+                            {
+                                file.CopyTo(fs);
+                                fs.Flush();
+                            }
+                            Response.StatusCode = 200;
+                        }
+                        else
+                        {
+                            Response.StatusCode = 204;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Response.Clear();
+                Response.ContentType = "application/json; charset=utf-8";
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = e.Message;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# blazor-server-rich-text-editor-image-upload
+
+This is the project to explain how we can use controller action with Rich Text Editor (Blazor Server App) to upload and save an image in the required destination.
+
+To run this project follow the below steps, 
+
+1. Clone this project.
+2. Open the project in Visual Studio 2019 or later.
+3. Build and run the project.
